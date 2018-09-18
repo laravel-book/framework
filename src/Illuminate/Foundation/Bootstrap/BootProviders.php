1b6e00c0906da42b5a29b497f05189c5bd966e07
Xiaohui Lam <xiaohuilam@users.noreply.github.com>
2018-09-18 15:28:03 +0800

    Update BootProviders.php

--- src/Illuminate/Foundation/Bootstrap/BootProviders.php
+++ src/Illuminate/Foundation/Bootstrap/BootProviders.php
@@ -9,11 +9,17 @@ class BootProviders
     /**
      * Bootstrap the given application.
      *
-     * @param  \Illuminate\Contracts\Foundation\Application  $app
+     * @param  \Illuminate\Foundation\Application  $app
      * @return void
      */
     public function bootstrap(Application $app)
     {
+
+        /**
+         * 启动服务提供者
+         *
+         * @see https://github.com/laravel-book/framework/blob/5.7/src/Illuminate/Foundation/Application.php#L791
+         */
         $app->boot();
     }
 }
