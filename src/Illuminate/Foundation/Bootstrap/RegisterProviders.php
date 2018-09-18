39df8e102a983a0e2ccf40ef941b326ead885df2
Xiaohui Lam <xiaohuilam@users.noreply.github.com>
2018-09-18 15:26:16 +0800

    Update RegisterProviders.php

--- src/Illuminate/Foundation/Bootstrap/RegisterProviders.php
+++ src/Illuminate/Foundation/Bootstrap/RegisterProviders.php
@@ -9,11 +9,16 @@ class RegisterProviders
     /**
      * Bootstrap the given application.
      *
-     * @param  \Illuminate\Contracts\Foundation\Application  $app
+     * @param  \Illuminate\Foundation\Application  $app
      * @return void
      */
     public function bootstrap(Application $app)
     {
+        /**
+         * 注册服务提供者
+         *
+         * @see https://github.com/laravel-book/framework/blob/5.7/src/Illuminate/Foundation/Application.php#L565
+         */
         $app->registerConfiguredProviders();
     }
 }
